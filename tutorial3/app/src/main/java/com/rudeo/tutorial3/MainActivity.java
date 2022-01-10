06cb2c1861cdad910eb630fd4e78ecd2a0157348
Kyungdae Park <rudeo7701@gmail.com>
2022-01-10 22:15:42 +0900

    [section] Alert & Dialog

---
+++ tutorial3/app/src/main/java/com/rudeo/tutorial3/MainActivity.java
@@ -0,0 +1,68 @@
+package com.rudeo.tutorial3;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] items = {"맥북", "아이폰" , "애플티비", "애플워치"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button listButton = (Button) findViewById(R.id.listButton);
+
+        listButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder =new AlertDialog.Builder(MainActivity.this);
+
+                builder.setTitle("리스트");
+                builder.setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast toast = Toast.makeText(MainActivity.this.getApplicationContext(),items[i], Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                });
+
+                AlertDialog alertDialog =  builder.create();
+                alertDialog.show();
+            }
+        });
+
+        Button quitButton = (Button) findViewById(R.id.quitButton);
+
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("정말로 종료하시겠습니까?");
+                builder.setTitle("종료 알림창")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+
+    }
+}
