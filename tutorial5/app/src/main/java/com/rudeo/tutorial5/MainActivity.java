2587b04e1fdc41ffc8724d8f7649fb5c440adc1e
Kyungdae Park <rudeo7701@gmail.com>
2022-01-11 23:22:38 +0900

    [section1] Custom ListView

---
+++ tutorial5/app/src/main/java/com/rudeo/tutorial5/MainActivity.java
@@ -0,0 +1,37 @@
+package com.rudeo.tutorial5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] items = {"망고 쥬스", "토마토 쥬스", "포도 쥬스"};
+        ListAdapter adapter = new ImageAdapter(this, items);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String item = String.valueOf(adapterView.getItemAtPosition(i));
+                Toast toast = Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT);
+                toast.show();
+
+
+            }
+        });
+    }
+}
